12bca27d34bd9cd484a252f4f6b19bc831765fa6
Abdulraheem <abdulraheemzulfiqar2232@gmail.com>
2024-07-02 22:51:27 +0500

    changes

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="max-w-3xl mx-auto p-4">
+      <body className={inter.className} >
+        <div className="max-w-3xl mx-auto p-4 ">
           <Navbar />
           <div className="mt-8">{children}</div>
         </div>
