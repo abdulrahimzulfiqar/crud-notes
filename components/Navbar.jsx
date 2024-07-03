c3f34128c99036a92fe79cacae6899545b820929
Abdulraheem <abdulraheemzulfiqar2232@gmail.com>
2024-07-03 20:03:40 +0500

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-      <Link href={"/"} className="text-white font-bold border-blue-500">
+      <Link href={"/"} className="text-gray-400 font-bold border-blue-500">
         GTCoding
       </Link>
       <Link href={"/addTopic"} className="bg-white text-blue-800 p-2">
